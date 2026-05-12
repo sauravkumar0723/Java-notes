c1c31d25ed30fd52636b89c591f1175fab2912bd
sauravkumar0723 <98612833+sauravkumar0723@users.noreply.github.com>
2026-05-12 15:58:43 +0530

    Custom Login Page, URL Security & In-Memory Auth

---
+++ JAVA/Spring Security/SpringBoot-9/src/main/java/tek/project/SpringSecurity/configuration/MySecurityConfig.java
@@ -0,0 +1,58 @@
+package tek.project.SpringSecurity.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class MySecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+		
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/test/admin").hasRole("ADMIN")
+		.antMatchers("/test/member").hasRole("MEMBER")
+		.anyRequest().permitAll()
+		.and()
+		.formLogin()
+		.loginPage("/test/mylogin")
+		.loginProcessingUrl("/doLogin")
+		.usernameParameter("user")
+		.passwordParameter("pass")
+		;
+		
+		return http.build();
+	}
+	
+	@Bean
+	InMemoryUserDetailsManager inMemory() {
+		
+		UserDetails admin =  User.builder()
+	  .username("admin")
+	  .password(passwordEncoder().encode("admin123") )
+	  .roles("ADMIN")
+	  .build();
+		
+	  UserDetails member =  User.builder()
+	  .username("member")
+	  .password(passwordEncoder().encode("member123") )
+	  .roles("MEMBER")
+	  .build();
+
+		return new InMemoryUserDetailsManager(admin,member);	
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ JAVA/Spring Security/SpringBoot-9/src/main/webapp/WEB-INF/view/login.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+        
+        <form action="/doLogin" method="post">
+               <input type="text" name="user" placeholder="Enter username"><br>
+               <input type="text" name="pass" placeholder="Enter Password"><br>
+               <button>login</button>
+        
+        
+        </form>
+        
+        
+</body>
+</html>
