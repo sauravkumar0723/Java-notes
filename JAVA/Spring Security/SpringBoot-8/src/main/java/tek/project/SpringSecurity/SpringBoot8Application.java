e6e65d02ad9b1cf46a83b531ca20d88b027a6fcd
sauravkumar0723 <98612833+sauravkumar0723@users.noreply.github.com>
2026-05-12 15:56:45 +0530

    Add files via upload
    Spring Security Basics & Form-Based Authentication 🔐| Authorization

---
+++ JAVA/Spring Security/SpringBoot-8/src/main/java/tek/project/SpringSecurity/SpringBoot8Application.java
@@ -0,0 +1,13 @@
+package tek.project.SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot8Application.class, args);
+	}
+
+}
---
+++ JAVA/Spring Security/SpringBoot-8/src/test/java/tek/project/SpringSecurity/SpringBoot8ApplicationTests.java
@@ -0,0 +1,13 @@
+package tek.project.SpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
